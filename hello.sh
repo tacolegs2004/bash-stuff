2fec5b9988039b7b20a3a585b1f648face1480d2
taco <tylerchall16@gmail.com>
2025-07-28 22:42:52 -0400

    test

--- hello.sh
+++ hello.sh
@@ -2,10 +2,13 @@
 
 name="Taco"
 
-
 # This is a test.
 echo "Hello, ${name}! You are ${age:=21} years old."
 echo "Hello, ${name}! You are ${age2:=22} years old."
 echo "$age is age"
 echo "$age2 is age2"
 echo "Hello, $#name!"
+
+while read -r num1 num2; do
+		echo "$num1 in $num2"
+done
